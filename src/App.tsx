c54dbcda5aa1133edba5e47ee96103e3c2f8cd91
Rehten <Mroow@yandex.ru>
2020-01-22 14:42:45 +0300

    add redux with thunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {PropsWithChildren} from 'react';
+import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import './App.css';
+import {AppState} from "./store";
+import {connect} from "react-redux";
+import {ThunkAction} from "redux-thunk";
+import {Action} from "redux";
+import {thunkSendMessage} from "./thunks";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props extends PropsWithChildren<{}> {
+    thunkSendMessage: (message: string) => ThunkAction<void, AppState, null, Action<string>>
 }
 
-export default App;
+const App = (props: Props) => {
+    return (
+        <Router>
+            <div>
+                <button onClick={() => {
+                    props.thunkSendMessage('MyMessage')
+                }}>test Redux
+                </button>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/foo">Foo</Link>
+                    <Link to="/bar">Bar</Link>
+                </nav>
+                <Switch>
+                    <Route exact path="/" component={() => <div>Home</div>}/>
+                    <Route exact path="/foo" component={() => <div>Foo</div>}/>
+                    <Route exact path="/bar" component={() => <div>Bar</div>}/>
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+const mapStateToProps = (state: AppState) => ({
+    system: state.system,
+    chat: state.chat
+});
+
+export default connect(
+    mapStateToProps,
+    {thunkSendMessage}
+)(App);
