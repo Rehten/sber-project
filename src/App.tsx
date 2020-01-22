7d85261bdc49ac7a6f79ff30592c4bca2f38afab
Rehten <Mroow@yandex.ru>
2020-01-22 15:14:33 +0300

    add redux with thunks

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import {connect} from "react-redux";
 import {ThunkAction} from "redux-thunk";
 import {Action} from "redux";
 import {thunkSendMessage} from "./thunks";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import List, { Item } from 'devextreme-react/list';
+
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.compact.css';
 
 interface Props extends PropsWithChildren<{}> {
     thunkSendMessage: (message: string) => ThunkAction<void, AppState, null, Action<string>>
@@ -15,10 +21,47 @@ const App = (props: Props) => {
     return (
         <Router>
             <div>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                        const errors: any = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Field type="email" name="email" />
+                            <ErrorMessage name="email" component="div" />
+                            <Field type="password" name="password" />
+                            <ErrorMessage name="password" component="div" />
+                            <button type="submit" disabled={isSubmitting}>
+                                Submit
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
                 <button onClick={() => {
                     props.thunkSendMessage('MyMessage')
                 }}>test Redux
                 </button>
+                <List>
+                    <Item>orange</Item>
+                    <Item>white</Item>
+                    <Item>black</Item>
+                </List>
                 <nav>
                     <Link to="/">Home</Link>
                     <Link to="/foo">Foo</Link>
